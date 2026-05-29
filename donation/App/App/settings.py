1f6ad772d7a29e1cd27a52ea397db58489516818
Anshul modi <anshulmodi79@gmail.com>
2026-05-29 19:01:45 +0530

    Added Allowed Host

--- donation/App/App/settings.py
+++ donation/App/App/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(vley37w53y9phsa7!(z=ub9vq-k2=l9ru99^*c_4g4g+q3!da
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
